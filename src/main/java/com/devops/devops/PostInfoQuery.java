55f6b594749b2846bde8747086798d3fcdc666b8
ayaan29 <mohd.ayaan.1251@gmail.com>
2025-02-12 10:08:11 +0530

    initial commit

---
+++ src/main/java/com/devops/devops/PostInfoQuery.java
@@ -0,0 +1,21 @@
+package com.devops.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+public class PostInfoQuery {
+	@GetMapping("/myposts/{uid}")
+	public PostMessage get(@PathVariable("uid") String uid) {
+		String URL = "https://jsonplaceholder.typicode.com/posts/"+uid;
+		
+		RestClient rc = RestClient.builder().build();	
+		
+		PostMessage result = rc.get().uri(URL).retrieve().body(PostMessage.class);
+//		System.out.println(result);
+		
+		return result;
+	}
+}
